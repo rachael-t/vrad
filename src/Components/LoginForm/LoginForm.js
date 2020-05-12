441087a0844d40b1729be460660d1246c40ef36b
Olivia Webster <oliviacweb@gmail.com>
2020-05-12 17:58:32 -0400

    Create handle method for login form

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -5,21 +5,40 @@ import './LoginForm.css';
 class LoginForm extends Component {
   constructor(props) {
     super()
+    this.state = {
+      userName: "",
+      userEmail: "",
+      userPurpose: ""
+    }
   }
+
+  handleChange = (event) => {
+    if (event.target.className === "name-input") {
+    this.setState( {userName: event.target.value} )
+  } else if (event.target.className === "email-input") {
+    this.setState( {userEmail: event.target.value} )
+  } else if (event.target.className === "purpose-selection") {
+    this.setState ( {userPurpose: event.target.value} )
+  }
+
+  }
+
+
 
   render() {
     return (
       <section className="login-section">
-      <h2>Welcome, please login!</h2>
+      <h2>Welcome, please log in!</h2>
       <form className="login-form">
-       <input className="name-input" type="text" placeholder="name"/>
-       <input className="email-input" type="text" placeholder="email"/>
-       <select className="purpose-selection">
+       <input className="name-input" type="text" placeholder="name" onChange={this.handleChange}/>
+       <input className="email-input" type="text" placeholder="email" onChange={this.handleChange}/>
+       <select className="purpose-selection" onChange={this.handleChange}>
         <option disabled selected value="what is your purpose">what is your purpose?</option>
         <option value="business">business</option>
         <option value="vacation">vacation</option>
         <option value="other">other</option>
        </select>
+       <button className="login-button">Log In</button>
       </form>
       </section>
     )
