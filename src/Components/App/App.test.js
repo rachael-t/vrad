fd375130e40270c1f7aa85bbf444aa45e2e64951
Rachael Thomas <rnthomas001@gmail.com>
2020-05-12 18:32:25 -0600

    Update App test file for future tests to this component

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it('Should render the app', () => {
+    // this was a test -- will edit since we are testing this in LoginForm.test.js
+    
+    // const { getByText } = render(<App />);
+    // expect(getByText('Welcome, please log in!')).toBeInTheDocument();
+  })
+})
