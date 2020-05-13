e208de2679c908965775d11c645748657dc4013a
Rachael Thomas <rnthomas001@gmail.com>
2020-05-13 15:49:36 -0600

    Add buttons and PropTypes

--- src/Components/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './UserProfile.css';
+import PropTypes from 'prop-types';
 
 const UserProfile = ({ name, purpose }) => {
     return (
         <div className='user-profile-container'>
             <h3>{`Hello, ${name}!`}</h3>
             <p>{`Reason for your Denver visit: ${purpose}`}</p>
-
+            <button className="user-favorites-button">
+                View Favorites
+            </button>
+            <button className="user-logout-button">
+                Log Out
+            </button>
         </div>
     )
 }
 
 export default UserProfile;
+
+UserProfile.propTypes = {
+    name: PropTypes.string,
+    purpose: PropTypes.string,
+}
