38f362e6029be2f8981066fff8c3ad3929e7f41c
Rachael Thomas <rnthomas001@gmail.com>
2020-05-12 18:33:51 -0600

    Change alert() to update the errorMessage state which is rendered to the component

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -8,12 +8,13 @@ class LoginForm extends Component {
     this.state = {
       userName: "",
       userEmail: "",
-      userPurpose: ""
+      userPurpose: "",
+      errorMessage: ""
     }
   }
 
   handleChange = (event) => {
-      if (event.target.className === "name-input") {
+    if (event.target.className === "name-input") {
       this.setState( {userName: event.target.value} )
     } else if (event.target.className === "email-input") {
       this.setState( {userEmail: event.target.value} )
@@ -23,33 +24,47 @@ class LoginForm extends Component {
   }
 
   handleSubmit = (event) => {
-    if (this.state.userName === "" || this.state.userEmail === "" ||
-    this.state.userPurpose === "") {
-      alert("Please fix this shit right now")
-    }
-    else {
-      event.preventDefault();
-      this.props.loginUser(this.state.userName, this.state.userPurpose)
-      alert("BAM")
+    event.preventDefault();
+    if (this.state.userName === "" || this.state.userEmail === "" || this.state.userPurpose === "") {
+      this.setState( {errorMessage: "Please fix this shit right now"} )
+    } else {
+      this.props.loginUser(this.state.userName, this.state.userPurpose);
+      //this will be changed in iteration 2:
+      this.setState( {errorMessage: 'WOOHOO'})
     }
   }
-
 
   render() {
     return (
       <section className="login-section">
-      <h2>Welcome, please log in!</h2>
-      <form className="login-form">
-       <input className="name-input" type="text" placeholder="name" onChange={this.handleChange}/>
-       <input className="email-input" type="text" placeholder="email" onChange={this.handleChange}/>
-       <select className="purpose-selection" onChange={this.handleChange}>
-        <option disabled selected value="what is your purpose">what is your purpose?</option>
-        <option value="business">business</option>
-        <option value="vacation">vacation</option>
-        <option value="other">other</option>
-       </select>
-       <button className="login-button" onClick={this.handleSubmit}>Log In</button>
-      </form>
+        <h2>Welcome, please log in!</h2>
+        <p className="login-error-message">{this.state.errorMessage}</p>
+        <form className="login-form">
+          <input 
+              className="name-input"
+              type="text"
+              placeholder="name"
+              onChange={this.handleChange}/>
+          <input 
+              className="email-input"
+              type="text"
+              placeholder="email"
+              onChange={this.handleChange}/>
+          <select 
+              className="purpose-selection"
+              placeholder='select your purpose'
+              onChange={this.handleChange}>
+            <option disabled selected value="what is your purpose">what is your purpose?</option>
+            <option value="business">business</option>
+            <option value="vacation">vacation</option>
+            <option value="other">other</option>
+          </select>
+          <button 
+            className="login-button"
+            onClick={this.handleSubmit}>
+              Log In
+              </button>
+        </form>
       </section>
     )
   }
