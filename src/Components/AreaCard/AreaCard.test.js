33ce5e36190e8cc73923735ea5110ff42a13b718
Olivia Webster <oliviacweb@gmail.com>
2020-05-14 21:00:46 -0400

    Add test for area card

---
+++ src/Components/AreaCard/AreaCard.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import AreaCard from './AreaCard';
+import '@testing-library/jest-dom';
+
+
+
+describe('AreaContainer', () => {
+    it('Should render the area card', () => {
+      const { getByText } = render(<AreaCard
+        about={ 'great area' }
+        id={ 303 }
+        key={ 303 }
+        listings={ [] }
+        name={ 'River North' }
+        shortName={ 'RiNo' }
+      />);
+      expect(getByText('great area')).toBeInTheDocument();
+      expect(getByText('River North')).toBeInTheDocument();
+      expect(getByText('Also known as RiNo')).toBeInTheDocument();
+    });
+  })
