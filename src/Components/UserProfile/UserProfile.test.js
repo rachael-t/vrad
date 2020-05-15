9a8d6996f9af1b181eb3a2ae0cd80c83f26758a7
Olivia Webster <oliviacweb@gmail.com>
2020-05-14 21:17:55 -0400

    Add test for log out button

--- src/Components/UserProfile/UserProfile.test.js
+++ src/Components/UserProfile/UserProfile.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import UserProfile from './UserProfile';
 import '@testing-library/jest-dom';
 import { BrowserRouter, Router } from 'react-router-dom';
@@ -18,4 +18,15 @@ describe('UserProfile', () => {
         expect(getByText('Reason for your visit to Denver: business')).toBeInTheDocument();
         expect(getByText('Log Out')).toBeInTheDocument();
       })
+     it('Should invoke logoutUser when user clicks the log out button', () => {
+       const mockLogoutUser = jest.fn();
+       const { getByText } = render(
+         <BrowserRouter>
+         <UserProfile name={'Olivia'}
+         purpose={'business'}
+         logoutUser={mockLogoutUser}/>
+       </BrowserRouter>);
+       fireEvent.click(getByText('Log Out'));
+       expect(mockLogoutUser).toHaveBeenCalledTimes(1);
+     })
     })
