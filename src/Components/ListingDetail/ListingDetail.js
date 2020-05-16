ffd5386a02ebf8fd51b30d5b91612f4e1cd418cd
Rachael Thomas <rnthomas001@gmail.com>
2020-05-16 16:14:48 -0600

    Create ListingDetail render JSX

---
+++ src/Components/ListingDetail/ListingDetail.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './ListingDetail.css';
+
+class ListingDetail extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      isFavorited: false,
+    }
+  }
+
+  render() {
+    console.log(this.props.listingDetails)
+    const { 
+        address,
+        areaId,
+        costPerNight,
+        listingId,
+        name,
+        numBathooms,
+        numBedrooms,
+        uniqueFeatures,
+        zipcode
+    } = this.props.listingDetails
+
+    return (
+      <div className={listingId}>
+        <h2>{name}</h2>
+        <p>Located at {address}, Denver, CO {zipcode}</p>
+        <p>ADD IMAGES</p>
+        <p>This rental is ${costPerNight} per night.</p>
+        <p>It has {numBedrooms} bedrooms and {numBathooms} bathrooms.</p>
+        <ul>Some unique features includes:</ul>
+            {uniqueFeatures.map(feature => {
+                return <li>{feature}</li>
+            })}
+      </div>
+    )
+  }
+}
+
+export default ListingDetail;
