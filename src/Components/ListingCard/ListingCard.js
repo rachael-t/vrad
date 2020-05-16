dc2b2d18634a44060229094e964e6692a64f60e0
Rachael Thomas <rnthomas001@gmail.com>
2020-05-16 16:14:24 -0600

    Create Link path for ListingDetails component

--- src/Components/ListingCard/ListingCard.js
+++ src/Components/ListingCard/ListingCard.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './ListingCard.css';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const ListingCard = (props) => {
+
     return (
         <div className='listing-card'>
             <h2>{props.name}</h2>
             <p>Image</p>
-            {/* <Link to={`/areas/${props.id}/listings/`}> */}
+            <Link 
+                to={`/areas/${props.areaId}/listings/${props.listingId}`}
+                onClick={() => props.viewListingDetails({...props})}
+            >
                 <button className='view-listing-details'>View Listing Details</button>
-            {/* </Link> */}
+            </Link>
         </div>
     )
+
 }
+
 
 export default ListingCard;
