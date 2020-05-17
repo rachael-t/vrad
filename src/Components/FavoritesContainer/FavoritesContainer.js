f82ac03d55ca3d47ee616cc4be1ce5a57c2c15db
Olivia Webster <oliviacweb@gmail.com>
2020-05-17 15:40:04 -0400

    rendered sample onto dom for favorites

---
+++ src/Components/FavoritesContainer/FavoritesContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+// import ListingCard from '../ListingCard/ListingCard';
+import './FavoritesContainer.css';
+
+const FavoritesContainer = () => {
+return (
+  <h1>this is ya faves</h1>
+)
+
+}
+export default FavoritesContainer;
