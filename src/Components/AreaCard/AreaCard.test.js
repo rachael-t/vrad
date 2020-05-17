be8ba6be6154f4d75d9034518009fe5edad9cdcb
Olivia Webster <oliviacweb@gmail.com>
2020-05-17 12:49:42 -0400

    wrap area card render in browser router to pass test

--- src/Components/AreaCard/AreaCard.test.js
+++ src/Components/AreaCard/AreaCard.test.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import AreaCard from './AreaCard';
 import '@testing-library/jest-dom';
+import { BrowserRouter, Router } from 'react-router-dom';
 
-describe('AreaContainer', () => {
+describe('AreaCard', () => {
     it('Should render the area card', () => {
-      const { getByText } = render(<AreaCard
+      const { getByText } = render(<BrowserRouter><AreaCard
         about={ 'great area' }
         id={ 303 }
         key={ 303 }
         listings={ [] }
         name={ 'River North' }
         shortName={ 'RiNo' }
-      />);
+      />
+      </BrowserRouter>);
       expect(getByText('great area')).toBeInTheDocument();
       expect(getByText('River North')).toBeInTheDocument();
       expect(getByText('Also known as RiNo')).toBeInTheDocument();
