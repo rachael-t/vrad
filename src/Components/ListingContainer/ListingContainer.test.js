73d18167bbf2c374aa3202a0ceac1ef2a44d465c
Olivia Webster <oliviacweb@gmail.com>
2020-05-17 12:51:11 -0400

    Add test to see if listing container renders

---
+++ src/Components/ListingContainer/ListingContainer.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ListingContainer from './ListingContainer';
+import '@testing-library/jest-dom';
+
+
+
+describe('ListingContainer', () => {
+    it('Should render the listing container', () => {
+      const { getByText } = render(<ListingContainer />);
+      expect(getByText('Area Listings')).toBeInTheDocument();
+    });
+  })
