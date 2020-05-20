9723cc20dd3574e3947a034d49117e208d7bdafb
Olivia Webster <oliviacweb@gmail.com>
2020-05-20 18:07:42 -0400

    Add and pass test for login form rendering on page load

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter, Router, MemoryRouter } from "react-router-dom";
 import App from './App';
+import LoginForm from '../LoginForm/LoginForm'
 
 describe('App', () => {
-  it('Should render the app', () => {
-    // this was a test -- will edit since we are testing this in LoginForm.test.js
-    
-    // const { getByText } = render(<App />);
-    // expect(getByText('Welcome, please log in!')).toBeInTheDocument();
+  it('Should render the header', () => {
+       const { getByText } = render(<BrowserRouter><App/> </BrowserRouter>)
+    expect(getByText('VRAD')).toBeInTheDocument();
+  })
+  it('Should render the login form', () => {
+    const { getByText } = render(<BrowserRouter>
+      <App  />
+      </ BrowserRouter>)
+      expect(getByText('Welcome, please log in!')).toBeInTheDocument();
   })
 })
