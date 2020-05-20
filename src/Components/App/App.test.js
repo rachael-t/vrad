7c8dded1f8c067cec8eb5d75a1b81bcf3c598c48
Olivia Webster <oliviacweb@gmail.com>
2020-05-20 18:18:12 -0400

    Add integration test for when user first logs in

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import { BrowserRouter, Router, MemoryRouter } from "react-router-dom";
 import App from './App';
 import LoginForm from '../LoginForm/LoginForm'
@@ -9,10 +9,30 @@ describe('App', () => {
        const { getByText } = render(<BrowserRouter><App/> </BrowserRouter>)
     expect(getByText('VRAD')).toBeInTheDocument();
   })
+
   it('Should render the login form', () => {
     const { getByText } = render(<BrowserRouter>
       <App  />
       </ BrowserRouter>)
       expect(getByText('Welcome, please log in!')).toBeInTheDocument();
+  })
+
+  it('Should display areas upon a succesful login', async() => {
+    const { getByText, getByPlaceholderText } = render(<BrowserRouter>
+      <App  />
+      </ BrowserRouter>)
+    fireEvent.change(getByPlaceholderText("name"), {
+      target: { value: "Olivia" },
+    });
+    fireEvent.change(getByPlaceholderText("email"), {
+      target: { value: "olivia@turing.com" },
+    });
+    fireEvent.change(getByPlaceholderText("select your purpose"), {
+      target: { value: "business" },
+    });
+    fireEvent.click(getByText("Log In"));
+    const neighborhood = await waitFor(() => getByText('Park Hill'))
+    expect(neighborhood).toBeInTheDocument();
+    expect(getByText('Denver Neighborhoods')).toBeInTheDocument();
   })
 })
